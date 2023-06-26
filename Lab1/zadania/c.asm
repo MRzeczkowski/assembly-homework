30d6ee17c7a7794370981649dbb096595bd5f0cb
MRzeczkowski <mateusz.rzeczkowski96@gmail.com>
2023-06-26 22:20:26 +0200

    c and d refactor

--- Lab1/zadania/c.asm
+++ Lab1/zadania/c.asm
@@ -1,47 +1,123 @@
 section .text
 org 100h
 start:
+	mov	dx, podaj_haslo
+	call wypisz
 
-	mov	dx, podajhaslo	
-	call drukuj_plus_endl
-	call czytaj_ciag_sekret
-	call nowa_linia
+	call wpisz_znaki_do_bufora
+	call wypisz_nowa_linia
+
 	call kopiuj_bufor
-	mov dx, sprawdzhaslo
-	call drukuj_plus_endl
-	call czytaj_ciag_sekret 
-	call nowa_linia
+
+	mov dx, powtorz_haslo
+	call wypisz
+
+	call wpisz_znaki_do_bufora
+	call wypisz_nowa_linia
+
 	call porownaj
-	
+
 	mov	ax, 4C00h
 	int	21h
-	
-	
-	;test - start
-drukuj_niepoprawne:
-	mov dx, haslo_niepoprawne
-	call drukuj_plus_endl
-	ret
-	
-zakoncz_porown:	
-	mov dx, haslo_poprawne
-	call drukuj_plus_endl
-	ret 
 
-porownaj_l:
-	cmp byte al, ah 
-	jnz drukuj_niepoprawne
-	cmp byte al, 24h
-	jz zakoncz_porown
-	inc si
-	inc di 
+wypisz:
+	mov	ah, 9
+	int	21h
+	ret
+
+wypisz_nowa_linia:
+	mov	dx, nowa_linia
+	call wypisz
+	ret
+
+wpisz_znaki_do_bufora:
+	call wyczysc_bufor
+	mov cx, [bufor_max_len]
+	mov bx, bufor
+	mov di, 0
+	mov dh, 13
+	jmp wpisz_znaki_do_bufora_petla
+
+wyczysc_bufor:
+	mov si, bufor
+	mov cx, 0
+	mov di, [bufor_max_len]
+	cmp di, cx
+	jnz wyczysc_bufor_petla
+	jz wyczysc_bufor_zakoncz
+
+wyczysc_bufor_petla:
+	mov bx, si
+	add bx, cx
+	mov byte [bx], 24h
+	inc cx
+	cmp di, cx
+	jnz wyczysc_bufor_petla
+	jz wyczysc_bufor_zakoncz
+
+wyczysc_bufor_zakoncz:
+	mov bx, bufor_len
+	mov word [bx], 0x0000
+	ret
+
+wpisz_znaki_do_bufora_petla:
+	call czytaj_znak
+	mov dl, [znak]
+	mov byte [bx], dl
+	cmp dh, dl
+	jz wpisano_enter
+	inc bx
+	inc di
+	cmp cx, di
+	jnz wpisz_znaki_do_bufora_petla
+	jz wpisz_znaki_do_bufora_zakoncz
+
+czytaj_znak:
+	mov ah, 07h ; no echo
+	int 21h
+	mov byte [znak], al
+	cmp al, 13
+	jnz pisz_gwiazdka
+	ret
+
+pisz_gwiazdka:
+	mov ah, 02h
+	mov dl, 0x2A
+	int 21h
+	ret
+
+wpisano_enter:
+	mov byte [bx], 24h
+	jmp wpisz_znaki_do_bufora_zakoncz
+
+wpisz_znaki_do_bufora_zakoncz:
+	mov [bufor_len], di
+	ret
+
+kopiuj_bufor:
+	mov si, bufor
+	mov bx, haslo
+	cmp byte [si], 24h
+	jnz kopiuj_bufor_petla
+	jz kopiuj_bufor_zakoncz
+
+kopiuj_bufor_petla:
 	mov byte al, [si]
-	mov byte ah, [di]
-	jmp porownaj_l
-	
+	mov byte [bx], al
+	inc bx
+	inc si
+	cmp byte [si], 24h
+	jnz kopiuj_bufor_petla
+	jz kopiuj_bufor_zakoncz
+
+kopiuj_bufor_zakoncz:
+	mov al, 24h
+	mov byte [bx], al
+	ret
+
 porownaj:
 	mov bx, haslo
-	mov si, bx 
+	mov si, bx
 	mov bx, bufor
 	mov di, bx
 	mov byte al, [si]
@@ -50,137 +126,37 @@ porownaj:
 	mov byte ah, [di]
 	cmp ah, 24h
 	jz drukuj_niepoprawne
-	jmp porownaj_l
-	
-	
-	
+	jmp porownaj_petla
 
-drukuj_plus_endl:
-	mov	ah, 9
-	int	21h
-	call nowa_linia
+drukuj_niepoprawne:
+	mov dx, haslo_niepoprawne
+	call wypisz
 	ret
 
-
-	
-nowa_linia: 
-	mov	ah, 2
-	mov	dl, 10
-	int	21h
-	mov	ah, 2
-	mov	dl, 13
-	int	21h
-	ret
-
-	
-czysc_bufor_l:
-	mov bx, si
-	add bx, cx
-	mov byte [bx], 24h
-	inc cx
-	cmp di, cx
-	jz czysc_bufor_after_loop
-	jnz czysc_bufor_l
-
-czysc_bufor:
-	mov si, bufor
-	mov cx, 0
-	mov di, [bufor_max_len]
-	cmp di, cx
-	jnz czysc_bufor_l
-	jz czysc_bufor_after_loop
-	
-czysc_bufor_after_loop:
-	mov bx, bufor_len
-	mov word [bx], 0x0000
-	ret
-	
-
-
-czytaj_ciag_sekret_after_loop:
-	mov [bufor_len], di
-	ret
-
-wpisano_enter:
-	mov byte [bx], 24h
-	jmp czytaj_ciag_sekret_after_loop
-
-czytaj_ciag_sekret_l:
-	call czytaj_znak_sekret
-	mov dl, [znak]
-	mov byte [bx], dl
-	cmp dh, dl
-	jz wpisano_enter
-	inc bx
-	inc di
-	cmp cx, di
-	jz czytaj_ciag_sekret_after_loop
-	jnz czytaj_ciag_sekret_l
-	
-
-czytaj_ciag_sekret:
-	call czysc_bufor
-	mov cx, [bufor_max_len]
-	mov bx, bufor
-	mov di, 0
-	mov dh, [carriage_return]
-	jmp czytaj_ciag_sekret_l
-
-
-kopiuj_bufor_after_loop:
-	mov al, 24h
-	mov byte [bx], al
-	ret
-	
-kopiuj_bufor_l:
-	mov byte al, [si]
-	mov byte [bx], al
-	inc bx
+porownaj_petla:
+	cmp byte al, ah
+	jnz drukuj_niepoprawne
+	cmp byte al, 24h
+	jz porownaj_zakoncz
 	inc si
-	cmp byte [si], 24h
-	jz kopiuj_bufor_after_loop
-	jnz kopiuj_bufor_l
-	
-kopiuj_bufor:
-	mov si, bufor
-	mov bx, haslo
-	cmp byte [si], 24h
-	jnz kopiuj_bufor_l
-	jz kopiuj_bufor_after_loop
+	inc di
+	mov byte al, [si]
+	mov byte ah, [di]
+	jmp porownaj_petla
 
-czytaj_znak_sekret:
-	mov ah, 07h
-	int 21h
-	mov byte [znak], al
-	cmp al, 13
-	jnz pisz_gwiazdka
-	;mov ah, 02h
-	;mov dl, 0x2A
-	;int 21h
-	ret
-	
-pisz_gwiazdka:
-	mov ah, 02h
-	mov dl, 0x2A
-	int 21h
+porownaj_zakoncz:
+	mov dx, haslo_poprawne
+	call wypisz
 	ret
 
 section .data
-
-	sprawdzhaslo db	"Sprawdz haslo$"
-	podajhaslo	db	"Podaj haslo$"
-	bufor		TIMES 51 db 24h ;--- H
-	haslo		TIMES 51 db 24h ; --- $
-	bufor_len	dw 0x0000
-	;haslo_len 	dw 0x0000
-	;haslo_max_len	dw 50
-	bufor_max_len	dw 50
-	znak			db 'a'
-	carriage_return db 13
-	adr_docel		dw 0x0000 
-	haslo_niepoprawne db "haslo niepoprawne$"
-	haslo_poprawne	  db "haslo poprawne$"
-	
-
-
-	
+	podaj_haslo			db	"Podaj haslo: $"
+	powtorz_haslo 		db	"Powtorz haslo: $"
+	nowa_linia 			db	13,10,"$"
+	bufor				TIMES 51 db 24h
+	bufor_len			dw 0x0000
+	bufor_max_len		dw 50
+	haslo				TIMES 51 db 24h
+	znak				db 'a'
+	haslo_poprawne		db "Haslo poprawne.$"
+	haslo_niepoprawne 	db "Haslo niepoprawne!$"
--- Lab1/zadania/d.asm
+++ Lab1/zadania/d.asm
@@ -1,67 +1,64 @@
 section .text
 org 100h
 start:
-	mov dx, msg
-	mov ah, 09h
-	int 21h
-	call nowa_linia
+	mov dx, podaj_sciezke
+	call wypisz
+	call wypisz_nowa_linia
+
 	mov byte [bufor], 64
 	mov byte [bufor + 1], 0
 	mov dx, bufor
 	mov ah, 0Ah
 	int 21h
+
 	mov al, [bufor + 1]
 	add al, 2
 	xor ah, ah 
 	mov si, ax 
 	mov byte [bufor + si], 0
-	call stworz_katalog
+
+	call utworz_katalog
+
 	mov	ax, 4C00h
 	int	21h
-	
 
-stworz_katalog_done:
+wypisz:
+	mov	ah, 9
+	int	21h
 	ret
-	
+
+wypisz_nowa_linia:
+	mov	dx, nowa_linia
+	call wypisz
+	ret
+
+utworz_katalog: 
+	mov cx, [bufor + 1]
+	mov si, bufor + 2
+	cmp cx, 0
+	jnz utworz_katalog_petla
+	ret
+
+utworz_katalog_petla:
+	cmp byte [si], '\'
+	jz nowy_kat
+	jmp przesun_indeksy
+
 nowy_kat:
 	mov byte [si], 0
 	mov dx, bufor + 2
 	mov ah, 39h 
 	int 21h
 	mov byte [si], '\'
-	jmp stworz_katalog_loop_2
 
-iteruj_dalej:
-	jmp stworz_katalog_loop_2
-	
-stworz_katalog_loop:
-	cmp byte [si], '\'
-	jz nowy_kat
-	jnz iteruj_dalej
-	
-stworz_katalog_loop_2:
+przesun_indeksy:
 	inc si
 	dec cx
 	cmp cx, 0
-	jz stworz_katalog_done
-	jnz stworz_katalog_loop
-	
-stworz_katalog: 
-	mov cx, [bufor + 1]
-	mov si, bufor + 2
-	cmp cx, 0
-	jnz stworz_katalog_loop
-	jz stworz_katalog_done
-	
-nowa_linia: 
-	mov	ah, 2
-	mov	dl, 10
-	int	21h
-	mov	ah, 2
-	mov	dl, 13
-	int	21h
+	jnz utworz_katalog_petla
 	ret
 
 section .data
-	bufor  	TIMES 63 db 0
-	msg		db "Podaj sciezke$"
+	bufor  				TIMES 63 db 0
+	podaj_sciezke		db "Podaj sciezke: $"
+	nowa_linia 			db	13,10,"$"
